775957dc011c61fb311149b857e29550f015ec29
Edmilbe <edmilbe14@hotmail.com>
2025-01-19 05:34:02 +0000

    Hide menu to anonnimous

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { Customer } from "../hooks/useCustomers";
 import useMe from "../hooks/useMe";
@@ -25,49 +25,55 @@ const Layout = () => {
             <h1 className="d-inline">Troca</h1>
           </a>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={() => {
-              setCollapsed(!collapsed);
-            }}
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className={`collapse navbar-collapse ${!collapsed ? "show" : ""}`}
-            id="navbarNav"
-          >
-            <div className="d-flex align-items-center w-100">
-              <ul className="navbar-nav flex-grow-1">
-                <li className="nav-item my-2">
-                  <Link className="nav-link" aria-current="page" to="/">
-                    Balanço
-                  </Link>
-                </li>
-                <li className="nav-item my-2">
-                  <Link className="nav-link" to="/friends">
-                    Amigos
-                  </Link>
-                </li>
-              </ul>
-              {data && !isLoading && (
-                <div className="d-block text-end">
-                  <button
-                    className="my-4 me-2 btn btn-outline-danger"
-                    onClick={handleLogout}
-                  >
-                    Sair de {data.first_name}
-                  </button>
+          {!isLoading && data && (
+            <React.Fragment>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+                onClick={() => {
+                  setCollapsed(!collapsed);
+                }}
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div
+                className={`collapse navbar-collapse ${
+                  !collapsed ? "show" : ""
+                }`}
+                id="navbarNav"
+              >
+                <div className="d-flex align-items-center w-100">
+                  <ul className="navbar-nav flex-grow-1">
+                    <li className="nav-item my-2">
+                      <Link className="nav-link" aria-current="page" to="/">
+                        Balanço
+                      </Link>
+                    </li>
+                    <li className="nav-item my-2">
+                      <Link className="nav-link" to="/friends">
+                        Amigos
+                      </Link>
+                    </li>
+                  </ul>
+                  {data && !isLoading && (
+                    <div className="d-block text-end">
+                      <button
+                        className="my-4 me-2 btn btn-outline-danger"
+                        onClick={handleLogout}
+                      >
+                        Sair de {data.first_name}
+                      </button>
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
-          </div>
+              </div>
+            </React.Fragment>
+          )}
         </div>
       </nav>
 
